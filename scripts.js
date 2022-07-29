e4eae6c7aa3d8c0e76c8c77c9fc232f393ef6fa3
jroncalli <jerryroncalli@gmail.com>
2022-07-29 14:08:26 -0700

    Completed basic signup form project

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 //jshint esversion: 6
-
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
+const listId = '0f991c4f80';
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -17,6 +19,44 @@ app.post('/', function (req, res) {
   let lastName = req.body.lName;
   let email = req.body.email;
   console.log(firstName, lastName, email);
+
+  const newSubscriber = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  mailchimp.setConfig({
+    apiKey: 'e4e92b5b435ff580818235a740d2ed2d',
+    server: 'us18',
+  });
+
+  async function run() {
+    const response = await mailchimp.lists
+      .addListMember(listId, {
+        email_address: newSubscriber.email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: newSubscriber.firstName,
+          LNAME: newSubscriber.lastName,
+        },
+      })
+      .then((response) => {
+        if (response.id !== '') {
+          res.sendFile(__dirname + '/success.html');
+        }
+      })
+      .catch((err) => {
+        res.sendFile(__dirname + '/failure.html');
+        console.log(err);
+      });
+  }
+
+  run();
+});
+
+app.post('/failure', function (req, res) {
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
